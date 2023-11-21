f0d5cf038b3a156031ba50c6f2f58e5a87cbebef
Álvaro Rubio <alruad@gmail.com>
2023-11-22 00:28:28 +0100

    Version 2.0 - Working

---
+++ Kindle Cover Fixer V2/DataGrid.cs
@@ -0,0 +1,23 @@
+﻿namespace Kindle_Cover_Fixer_V2
+{
+    public struct DataGridUserCols
+    {
+        public string FileNumber { get; set; }
+        public string FileName { get; set; }
+        public string FileUuid { get; set; }
+        public string FileCan {  get; set; }
+    }
+    public struct DataGridSystemCols
+    {
+        public int FileNumber { get; set; }
+        public string FilePath { get; set; }
+        public string FileUuid { get; set; }
+        public string FileName { get; set; }
+    }
+    public struct DataGridTransferCols
+    {
+        public string FileNumber { get; set; }
+        public string FileName { get; set; }
+        public string FileSize { get; set; }
+    }
+}
---
+++ Kindle Cover Fixer V2/InfoData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace Kindle_Cover_Fixer_V2
+{
+
+    public class UsefulVariables
+    {
+        // Define the application version
+        public const string AppVersion = "2.0";
+        // Determine the %APPDATA% folder
+        public static string OutputFolder()
+        {
+            string? appData = Environment.GetEnvironmentVariable("APPDATA");
+            string result = appData + @"\Kindle Cover Fixer\Output";
+            if(appData != null) 
+            {
+                return result;
+            }
+            else
+            {
+                return Environment.CurrentDirectory + @"\Output";
+            }    
+        }
+        // Determine where is the Calibre configuration file
+        public static string[] CalibreLibraries()
+        {
+            List<string> resultList = new();
+            string? path = Environment.GetEnvironmentVariable("APPDATA");
+            string configFile;
+            if (path?.Length > 0)
+            {  
+                configFile = path + @"\calibre\gui.json";
+                string text = File.ReadAllText(configFile);
+                JObject? libJson = JObject.Parse(text);
+                JToken? libJsonRes = libJson["library_usage_stats"];
+                if (libJsonRes is not null)
+                {
+                    string[] lines = libJsonRes.ToString().Split(Environment.NewLine.ToCharArray());
+                    foreach (string line in lines)
+                    {
+                        if (line != string.Empty && line != "}" && line != "{")
+                        {
+                            resultList.Add(line.Split(": ")[0].Split('"')[1]);
+                        }                       
+                    }
+                }
+                else
+                {
+                    resultList.Add("[ERROR] Cant find Calibre Libraries in its configuration files.");
+                }       
+            }
+            else
+            {
+                resultList.Add("[ERROR] Cant find where is the APPDATA folder in the system.");
+            }
+            string[] result = resultList.ToArray();
+            return result;
+        }
+    }
+
+}
